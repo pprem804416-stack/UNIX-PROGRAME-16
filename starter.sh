699ff4cc4fce3b5c3ca5fc1e221b75b5d18ded0b
pprem804416-stack <pprem804416@gmail.com>
2026-09-22 11:17:42 +0530

    UNIX PROGRAM 16

--- starter.sh
+++ starter.sh
@@ -1,1 +1,14 @@
+# Create developers group
+groupadd developers
 
+# Check group
+grep -w developers /etc/group
+
+# Display user information
+cat /etc/passwd
+
+# Add user to developers group
+usermod -aG developers avahi
+
+# Verify group membership
+grep -w developers /etc/group
