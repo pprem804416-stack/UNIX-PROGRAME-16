51a20f20a921ba4091dbf16e1e0ef28829cc9902
pprem804416-stack <pprem804416@gmail.com>
2026-09-22 11:21:40 +0530

    UNIX PROGRAM 16

--- starter.sh
+++ starter.sh
@@ -1,1 +1,10 @@
-user_group_management.sh
+# Linux User and Group Management
+
+## Commands
+
+```bash
+groupadd developers
+grep -w developers /etc/group
+cat /etc/passwd
+usermod -aG developers auahi
+grep -w developers /etc/group
